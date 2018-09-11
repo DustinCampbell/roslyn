1782144195c6a11a0feb9e798043b6543b7da68a
Dustin Campbell <dustin@teamcampbell.org>
2018-09-11 09:24:58 -0700

    Factor IEqualityComparer<T> for ImmutableHashSet into a utility class
    CompletionServiceWithProviders directly implemented IEqualityComparer<ImmutableHashSet<string>>
    to handle comparison between hash sets for text view roles. This change removes that implementation
    and factors it into a utility class to allow it to be used by other similar features (e.g. Signature
    Help). This is technically a breaking change, but it's very unlikely that any consumers of
    CompletionService would depend on the fact that one of its subtypes implemented
    IEqualityComparer<ImmutableHashSet<string>>.

--- src/Features/Core/Portable/Completion/CompletionServiceWithProviders.cs
+++ src/Features/Core/Portable/Completion/CompletionServiceWithProviders.cs
@@ -20,7 +20,7 @@ namespace Microsoft.CodeAnalysis.Completion
     /// <summary>
     /// A subtype of <see cref="CompletionService"/> that aggregates completions from one or more <see cref="CompletionProvider"/>s.
     /// </summary>
-    public abstract class CompletionServiceWithProviders : CompletionService, IEqualityComparer<ImmutableHashSet<string>>
+    public abstract class CompletionServiceWithProviders : CompletionService
     {
         private static readonly Func<string, List<CompletionItem>> s_createList = _ => new List<CompletionItem>();
 
@@ -51,7 +51,7 @@ namespace Microsoft.CodeAnalysis.Completion
         {
             _workspace = workspace;
             ExclusiveProviders = exclusiveProviders;
-            _rolesToProviders = new Dictionary<ImmutableHashSet<string>, ImmutableArray<CompletionProvider>>(this);
+            _rolesToProviders = new Dictionary<ImmutableHashSet<string>, ImmutableArray<CompletionProvider>>(HashSetEqualityComparer<string>.Instance);
             _createRoleProviders = CreateRoleProviders;
             _getProviderByName = GetProviderByName;
         }
@@ -502,40 +502,6 @@ namespace Microsoft.CodeAnalysis.Completion
             {
                 return CompletionChange.Create(new TextChange(item.Span, item.DisplayText));
             }
-        }
-
-        bool IEqualityComparer<ImmutableHashSet<string>>.Equals(ImmutableHashSet<string> x, ImmutableHashSet<string> y)
-        {
-            if (x == y)
-            {
-                return true;
-            }
-
-            if (x.Count != y.Count)
-            {
-                return false;
-            }
-
-            foreach (var v in x)
-            {
-                if (!y.Contains(v))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        int IEqualityComparer<ImmutableHashSet<string>>.GetHashCode(ImmutableHashSet<string> obj)
-        {
-            var hash = 0;
-            foreach (var o in obj)
-            {
-                hash += o.GetHashCode();
-            }
-
-            return hash;
         }
     }
 }
---
+++ src/Workspaces/Core/Portable/Utilities/HashSetEqualityComparer.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Roslyn.Utilities
+{
+    internal class HashSetEqualityComparer<T> : IEqualityComparer<ImmutableHashSet<T>>
+    {
+        public static HashSetEqualityComparer<T> Instance { get; } = new HashSetEqualityComparer<T>();
+
+        public bool Equals(ImmutableHashSet<T> x, ImmutableHashSet<T> y)
+        {
+            if (x == y)
+            {
+                return true;
+            }
+
+            if (x.Count != y.Count)
+            {
+                return false;
+            }
+
+            foreach (var v in x)
+            {
+                if (!y.Contains(v))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int GetHashCode(ImmutableHashSet<T> obj)
+        {
+            var hash = 0;
+            foreach (var o in obj)
+            {
+                unchecked
+                {
+                    hash += o.GetHashCode();
+                }
+            }
+
+            return hash;
+        }
+    }
+}
